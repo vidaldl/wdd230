92e4ef54dae562de0beae99a0409bfe21c03bbea
David Vidal <vidaldl@outlook.com>
2022-06-24 23:25:47 -0600

    added day counter

--- assignments/chamber/scripts/main.js
+++ assignments/chamber/scripts/main.js
@@ -18,4 +18,32 @@ if(date.includes("Monday") || date.includes("Tuesday")) {
 }
 
 
+function getLastVisited() {
+  const today = new Date();
+
+  const todayDisplay = document.getElementById("last-visit");
+ 
+// get the stored value in localStorage
+let lastDate;
+
+if(window.localStorage.getItem("visits-last") === null) {
+  lastDate = new Date();
+} else {
+  lastDate = new Date(window.localStorage.getItem("visits-last"));
+}
+
+let dayDiff = Math.abs(today - lastDate);
+let diffDays = Math.ceil(dayDiff/(1000 * 60 * 60 * 24));
+console.log(diffDays)
+todayDisplay.innerText = diffDays + " days ago";
+
+
+// store the new number of visits value
+localStorage.setItem("visits-last", today);
+}
+
+
+getLastVisited();
+
+
 
